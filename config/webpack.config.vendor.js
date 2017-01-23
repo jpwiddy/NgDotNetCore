45cd937fbabdf44cb75e3d7368f6c25c2f7a5273
Jake Widmer <jpwiddy@gmail.com>
2017-01-23 09:48:04 -0600

    updates

--- config/webpack.config.vendor.js
+++ config/webpack.config.vendor.js
@@ -16,27 +16,11 @@ module.exports = {
         ]
     },
     entry: {
-        // polyfills: [
-        //     'core-js/es6/symbol',
-        //     'core-js/es6/object',
-        //     'core-js/es6/function',
-        //     'core-js/es6/parse-int',
-        //     'core-js/es6/parse-float',
-        //     'core-js/es6/number',
-        //     'core-js/es6/math',
-        //     'core-js/es6/string',
-        //     'core-js/es6/date',
-        //     'core-js/es6/array',
-        //     'core-js/es6/regexp',
-        //     'core-js/es6/map',
-        //     'core-js/es6/set',
-        //     'core-js/es6/weak-map',
-        //     'core-js/es6/weak-set',
-        //     'core-js/es6/typed',
-        //     'core-js/es6/reflect',
-        //     'core-js/es7/reflect',
-        //     'zone.js/dist/zone'
-        // ],
+        polyfills: [
+            'core-js/client/shim',
+            'zone.js/dist/zone',
+            'reflect-metadata'
+        ],
         vendor: [
             'font-awesome/scss/font-awesome.scss',
             'bootstrap/scss/bootstrap.scss',
@@ -50,8 +34,7 @@ module.exports = {
             '@angular/router',
             'rxjs',
             '@ng-bootstrap/ng-bootstrap',
-            'zone.js',
-            'reflect-metadata'
+            'zone.js'
         ]
     },
     output: {
